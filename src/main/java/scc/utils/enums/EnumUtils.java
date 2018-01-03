770df9977d6d27c26a97f0a820f13a51c05a5f0b
Kamil <blueflamesxcode@gmail.com>
2018-01-03 14:59:09 +0100

    add EnumUtils class

---
+++ src/main/java/scc/utils/enums/EnumUtils.java
@@ -0,0 +1,37 @@
+package scc.utils.enums;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.BiPredicate;
+
+public class EnumUtils<E extends Enum<E>> {
+    private static final Set<Class> approvedClasses = new HashSet<>();
+
+    public Optional<E> getValue(Class<E> givenClass, final String identity) {
+        optimizedValidation(givenClass);
+        E[] enumConstants = givenClass.getEnumConstants();
+
+        return findEnum(enumConstants, identity);
+    }
+
+    private void optimizedValidation(Class<E> givenClass) {
+        if (!approvedClasses.contains(givenClass)) {
+
+            if (!givenClass.isEnum())
+                throw new IllegalArgumentException("Given class is not enum");
+
+            approvedClasses.add(givenClass);
+        }
+    }
+
+    private Optional<E> findEnum(E[] enumConstants, final String givenName) {
+        BiPredicate<E, String> isEqualByName = (enumConst, identity) -> enumConst.toString()
+                                                                                 .equals(identity);
+
+        return Arrays.stream(enumConstants)
+                .filter(e -> isEqualByName.test(e, givenName))
+                .findFirst();
+    }
+}
