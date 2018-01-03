71e2bd07254031528956313c2e26d9e9281ddf10
Kamil <blueflamesxcode@gmail.com>
2018-01-03 15:00:35 +0100

    add getByName method

--- src/main/java/scc/converterFunctionality/OutputFormat.java
+++ src/main/java/scc/converterFunctionality/OutputFormat.java
@@ -1,7 +1,17 @@
 package scc.converterFunctionality;
 
+import scc.utils.enums.EnumUtils;
+
+import java.util.Optional;
+
 public enum OutputFormat {
     JSON,
     XML,
     TABLE;
+
+    private static EnumUtils<OutputFormat> enumUtils = new EnumUtils<>();
+
+    public static Optional<OutputFormat> getByName(String name) {
+        return enumUtils.getValue(OutputFormat.class, name);
+    }
 }
