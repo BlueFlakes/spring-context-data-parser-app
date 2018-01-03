ad4c37feafcaff90dd57a6e5a06f2dffff681ac6
Kamil <blueflamesxcode@gmail.com>
2018-01-03 12:58:03 +0100

    Add FileReader class

---
+++ src/main/java/scc/utils/dao/FileReader.java
@@ -0,0 +1,11 @@
+package scc.utils.dao;
+
+import java.util.Iterator;
+
+public class FileReader {
+
+    public Iterator<String> readData(String path) {
+        System.out.println("read data from file");
+        return null;
+    }
+}
