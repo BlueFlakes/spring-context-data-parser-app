59697f0df86eb13b25cf69b0a34b44f5345ce831
Kamil <blueflamesxcode@gmail.com>
2018-01-04 13:22:06 +0100

    Impl FileReader

--- src/main/java/scc/dao/FileReader.java
+++ src/main/java/scc/dao/FileReader.java
@@ -1,21 +1,36 @@
-package scc.utils.dao;
+package scc.dao;
+
+import scc.exception.DAOException;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
 
 public class FileReader {
+
+    public Iterator<String> readData(String path) throws DAOException {
+
+        try (Scanner file = openFile(path)) {
+            List<String> retrievedData = readFile(file);
+            return retrievedData.iterator();
+
+        } catch (FileNotFoundException e) {
+            throw new DAOException("FileReader couldn't find expected file in given path.");
+        }
+    }
 
     private Scanner openFile(String filename) throws FileNotFoundException {
         return new Scanner(new File(filename));
     }
 
-    public Iterator<String> readData(String path) {
-        System.out.println("read data from file");
+    private List<String> readFile(Scanner file) {
+        List<String> tempDataContainer = new ArrayList<>();
 
-        return null;
+        while (file.hasNextLine()) {
+            String line = file.nextLine();
+            tempDataContainer.add(line);
+        }
+
+        return tempDataContainer;
     }
-
-
 }
