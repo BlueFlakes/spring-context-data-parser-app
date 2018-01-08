4d93863d87e328852e53f20be04fd527f4f41fe0
Kamil <blueflamesxcode@gmail.com>
2018-01-08 12:32:23 +0100

    Impl start method inside OneArgStarter

--- src/main/java/scc/controller/OneArgStarter.java
+++ src/main/java/scc/controller/OneArgStarter.java
@@ -1,5 +1,12 @@
 package scc.controller;
 
+import scc.dao.CsvReader;
+import scc.dao.FileReader;
+import scc.models.DataProcessorCreator;
+import scc.models.SimpleCsvConverter;
+import scc.services.converterServices.formatter.OutputFormatterFactory;
+import scc.services.converterServices.printer.OutputPrinterFactory;
+
 public class OneArgStarter implements ConverterStarter {
     private String pathToFile;
 
@@ -9,6 +16,16 @@ public class OneArgStarter implements ConverterStarter {
 
     @Override
     public void start( ) throws Exception {
+        FileReader fileReader = new FileReader();
+        String defaultDelimiter = ",";
+        CsvReader csvReader = new CsvReader(fileReader, defaultDelimiter);
 
+        OutputFormatterFactory formatterFactory = new OutputFormatterFactory();
+        OutputPrinterFactory printerFactory = new OutputPrinterFactory();
+        DataProcessorCreator dataProcessorCreator = new DataProcessorCreator(formatterFactory, printerFactory);
+
+        SimpleCsvConverter simpleCsvConverter = new SimpleCsvConverter(csvReader, dataProcessorCreator);
+
+        simpleCsvConverter.convert(this.pathToFile);
     }
 }
