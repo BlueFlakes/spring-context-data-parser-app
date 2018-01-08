bacbe854f94ce91ddd8dd4d28a265174966f053c
Kamil <blueflamesxcode@gmail.com>
2018-01-08 21:55:08 +0100

    Change dir for SimpleConverter

---
+++ src/main/java/scc/controller/SimpleConverter.java
@@ -0,0 +1,39 @@
+package scc.controller;
+
+import scc.dao.DataLoader;
+import scc.exception.DAOException;
+import scc.exception.InvalidOutputFormatterException;
+import scc.exception.InvalidOutputPrinterException;
+import scc.models.DataProcessorCreator;
+
+import java.util.List;
+
+public class SimpleConverter {
+    private final DataLoader dataLoader;
+    private final DataProcessorCreator dataProcessorCreator;
+
+    public SimpleConverter(DataLoader dataLoader, DataProcessorCreator dataProcessorCreator) {
+        this.dataLoader = dataLoader;
+        this.dataProcessorCreator = dataProcessorCreator;
+    }
+
+    public void convert(DataProcessorCreator.ProcessorBuildingBlocks processorBuildingBlocks)
+            throws DAOException, InvalidOutputFormatterException, InvalidOutputPrinterException {
+
+        DataProcessorCreator.DataProcessor dataProcessor =
+                this.dataProcessorCreator.createDataProcessor(processorBuildingBlocks);
+        handleConversion(dataProcessor);
+    }
+
+    public void convert()
+            throws DAOException, InvalidOutputFormatterException, InvalidOutputPrinterException {
+
+        DataProcessorCreator.DataProcessor dataProcessor = this.dataProcessorCreator.createDefaultDataProcessor();
+        handleConversion(dataProcessor);
+    }
+
+    private void handleConversion(DataProcessorCreator.DataProcessor dataProcessor) throws DAOException {
+        List<String[]> loadedData = this.dataLoader.getResourceContent();
+        dataProcessor.process(loadedData);
+    }
+}
