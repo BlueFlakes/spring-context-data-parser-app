8d6d548e2af933dff2fae66a7a4e1c4c72c1cbe8
Kamil <blueflamesxcode@gmail.com>
2018-01-08 19:52:04 +0100

    format code inside file

--- src/main/java/scc/controller/ConverterStarterFactory.java
+++ src/main/java/scc/controller/ConverterStarterFactory.java
@@ -7,10 +7,12 @@ public class ConverterStarterFactory {
         switch (args.length) {
             case 0:
                 return new ZeroArgStarter();
+
             case 1:
                 return new OneArgStarter(args);
+
             case 2:
-                return new TwoArgsStarter(args);
+                return new ManyArgsStarter(args);
 
             default:
                 throw new InvalidArgsAmountException("Incorrect amount of arguments delivered.");
