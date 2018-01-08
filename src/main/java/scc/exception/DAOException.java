af215bef212b058654e02ddaf2814446b7db449e
Kamil <blueflamesxcode@gmail.com>
2018-01-08 09:51:26 +0100

    delete constructor with throwable in DAOException

--- src/main/java/scc/exception/DAOException.java
+++ src/main/java/scc/exception/DAOException.java
@@ -4,8 +4,4 @@ public class DAOException extends Exception {
     public DAOException(String message) {
         super(message);
     }
-
-    public DAOException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
