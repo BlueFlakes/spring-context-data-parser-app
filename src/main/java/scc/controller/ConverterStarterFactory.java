274fdf0c852c77cb652c9eab5f93537903c7ea7c
Kamil <blueflamesxcode@gmail.com>
2018-01-08 09:54:26 +0100

    Add factory for starter classes

---
+++ src/main/java/scc/controller/ConverterStarterFactory.java
@@ -0,0 +1,19 @@
+package scc.controller;
+
+import scc.exception.InvalidArgsAmountException;
+
+public class ConverterStarterFactory {
+    public ConverterStarter getProperStarter(String[] args) throws InvalidArgsAmountException {
+        switch (args.length) {
+            case 0:
+                return new ZeroArgStarter();
+            case 1:
+                return new OneArgStarter(args);
+            case 2:
+                return new TwoArgsStarter(args);
+
+            default:
+                throw new InvalidArgsAmountException("Incorrect amount of arguments delivered.");
+        }
+    }
+}
