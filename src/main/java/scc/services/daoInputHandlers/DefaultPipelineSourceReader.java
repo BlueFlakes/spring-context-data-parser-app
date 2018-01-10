e134b3e626a0e46cbd08fc4e8dfe067adc8df1e5
Kamil <blueflamesxcode@gmail.com>
2018-01-10 12:08:43 +0100

    Reduce ManyInputsHandler And Refactor OneInputHandler name

--- src/main/java/scc/services/daoInputHandlers/DefaultPipelineSourceReader.java
+++ src/main/java/scc/services/daoInputHandlers/DefaultPipelineSourceReader.java
@@ -10,20 +10,20 @@ import scc.services.document.Document;
 import scc.services.document.DocumentCreator;
 import scc.services.document.DocumentCreatorFactory;
 
-public class OneInputHandler implements DaoInputHandler {
+public class DefaultPipelineSourceReader implements DaoInputHandler {
     private final DataLoaderType dataLoaderType = DataLoaderType.CSV_READER;
     private ReaderCreatorFactory readerCreatorFactory;
     private DocumentCreatorFactory documentCreatorFactory;
 
-    public OneInputHandler(ReaderCreatorFactory readerCreatorFactory,
-                           DocumentCreatorFactory documentCreatorFactory) {
+    public DefaultPipelineSourceReader(ReaderCreatorFactory readerCreatorFactory,
+                                       DocumentCreatorFactory documentCreatorFactory) {
 
         this.readerCreatorFactory = readerCreatorFactory;
         this.documentCreatorFactory = documentCreatorFactory;
     }
 
     @Override
-    public Document prepareData(ArgsInterpreter argsInterpreter ) throws DAOException {
+    public Document prepareData(ArgsInterpreter argsInterpreter) throws DAOException {
         ReaderSettingsHandler readerSettingsHandler =
                 readerCreatorFactory.getReaderCreatorByDataSource(this.dataLoaderType, argsInterpreter);
 
