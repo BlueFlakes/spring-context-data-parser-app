8c3192c1d55d0203de2e4342bd5141f55211ceab
Kamil <blueflamesxcode@gmail.com>
2018-01-10 11:07:53 +0100

    Add ManyInputsHandler class

---
+++ src/main/java/scc/services/daoInputHandlers/ManyInputsHandler.java
@@ -0,0 +1,15 @@
+package scc.services.daoInputHandlers;
+
+import scc.exception.DAOException;
+import scc.models.ArgsInterpreter;
+import scc.services.document.Document;
+
+public class ManyInputsHandler implements DaoInputHandler {
+
+
+
+    @Override
+    public Document prepareData(ArgsInterpreter argsInterpreter) throws DAOException {
+        return null;
+    }
+}
