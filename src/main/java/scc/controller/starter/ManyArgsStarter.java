db29146bbbb88174e7e6b4982741d5f23ccd0d11
Kamil <blueflamesxcode@gmail.com>
2018-01-10 12:11:37 +0100

    Impl ManyArgsStarter

--- src/main/java/scc/controller/starter/ManyArgsStarter.java
+++ src/main/java/scc/controller/starter/ManyArgsStarter.java
@@ -1,6 +1,18 @@
 package scc.controller.starter;
 
+import scc.controller.SimpleConverter;
+import scc.dao.ReaderCreatorFactory;
+import scc.enums.OutputFormat;
+import scc.enums.PrinterType;
 import scc.models.ArgsInterpreter;
+import scc.models.CsvArgsInterpreter;
+import scc.models.DataProcessorBuildingBlocks;
+import scc.models.DataProcessorCreator;
+import scc.services.daoInputHandlers.DaoInputHandler;
+import scc.services.daoInputHandlers.DefaultPipelineSourceReader;
+import scc.services.document.DocumentCreatorFactory;
+import scc.services.formatter.OutputFormatterFactory;
+import scc.services.printer.OutputPrinterFactory;
 
 public class ManyArgsStarter implements ConverterStarter {
     private ArgsInterpreter argsInterpreter;
@@ -11,6 +23,17 @@ public class ManyArgsStarter implements ConverterStarter {
 
     @Override
     public void start( ) throws Exception {
+        DocumentCreatorFactory documentCreatorFactory = new DocumentCreatorFactory();
+        ReaderCreatorFactory readerCreatorFactory = new ReaderCreatorFactory();
+        DaoInputHandler daoInputHandler = new DefaultPipelineSourceReader(readerCreatorFactory, documentCreatorFactory);
 
+        OutputFormatterFactory formatterFactory = new OutputFormatterFactory();
+        OutputPrinterFactory printerFactory = new OutputPrinterFactory();
+        DataProcessorCreator dataProcessorCreator = new DataProcessorCreator(formatterFactory, printerFactory);
+
+        SimpleConverter simpleConverter = new SimpleConverter(daoInputHandler, dataProcessorCreator);
+        CsvArgsInterpreter csvArgsInterpreter = new CsvArgsInterpreter(this.argsInterpreter);
+        OutputFormat outputFormat = csvArgsInterpreter.getOutputFormat();
+        simpleConverter.convert(argsInterpreter, new DataProcessorBuildingBlocks(outputFormat, PrinterType.PRINT_TO_CONSOLE));
     }
 }
