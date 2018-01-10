fd5293fdc5e565f8d68e993936ea7ac850a17467
Kamil <blueflamesxcode@gmail.com>
2018-01-10 12:09:49 +0100

    Add getOutputFormat method from args

--- src/main/java/scc/models/CsvArgsInterpreter.java
+++ src/main/java/scc/models/CsvArgsInterpreter.java
@@ -1,5 +1,8 @@
 package scc.models;
 
+import scc.enums.OutputFormat;
+import scc.exception.InvalidOutputFormatterException;
+
 import java.util.List;
 
 public class CsvArgsInterpreter {
@@ -13,6 +16,29 @@ public class CsvArgsInterpreter {
         List<String> basicSettings = this.argsInterpreter.getBasicSettings();
         int settingsAmount = basicSettings.size();
 
-        return settingsAmount >= 2 ? basicSettings.get(1) : basicSettings.get(0);
+        return settingsAmount == 2 ? basicSettings.get(1) : basicSettings.get(0);
+    }
+
+    private String getExpectedOutputFormatName() {
+        List<String> basicSettings = this.argsInterpreter.getBasicSettings();
+        int settingsAmount = basicSettings.size();
+
+        return settingsAmount == 2 ? basicSettings.get(0) : null;
+    }
+
+    public OutputFormat getOutputFormat() throws InvalidOutputFormatterException {
+        String expectedOutputFormat = getExpectedOutputFormatName();
+
+        if (expectedOutputFormat != null) {
+            String upperCasedName = expectedOutputFormat.toUpperCase();
+            OutputFormat outputFormat = OutputFormat.getByName(upperCasedName);
+
+            if (outputFormat != null)
+                return outputFormat;
+
+            throw new InvalidOutputFormatterException("Incorrect name delivered.");
+        }
+
+        return OutputFormat.TABLE;
     }
 }
