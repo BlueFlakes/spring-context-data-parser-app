8f7a330c99fecb1e2e744d3d33814bddc79e0d62
Kamil <blueflamesxcode@gmail.com>
2018-01-10 12:10:17 +0100

    Adapt for using class under new name

--- src/main/java/scc/controller/starter/OneArgStarter.java
+++ src/main/java/scc/controller/starter/OneArgStarter.java
@@ -5,7 +5,7 @@ import scc.dao.ReaderCreatorFactory;
 import scc.services.daoInputHandlers.DaoInputHandler;
 import scc.models.ArgsInterpreter;
 import scc.models.DataProcessorCreator;
-import scc.services.daoInputHandlers.OneInputHandler;
+import scc.services.daoInputHandlers.DefaultPipelineSourceReader;
 import scc.services.document.DocumentCreatorFactory;
 import scc.services.formatter.OutputFormatterFactory;
 import scc.services.printer.OutputPrinterFactory;
@@ -22,7 +22,7 @@ public class OneArgStarter implements ConverterStarter {
 
         DocumentCreatorFactory documentCreatorFactory = new DocumentCreatorFactory();
         ReaderCreatorFactory readerCreatorFactory = new ReaderCreatorFactory();
-        DaoInputHandler daoInputHandler = new OneInputHandler(readerCreatorFactory, documentCreatorFactory);
+        DaoInputHandler daoInputHandler = new DefaultPipelineSourceReader(readerCreatorFactory, documentCreatorFactory);
 
         OutputFormatterFactory formatterFactory = new OutputFormatterFactory();
         OutputPrinterFactory printerFactory = new OutputPrinterFactory();
