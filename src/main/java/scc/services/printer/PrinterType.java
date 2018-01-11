20164c31f1a85dd6622bf6db321fa6b588d9b5c2
Kamil <blueflamesxcode@gmail.com>
2018-01-11 12:37:46 +0100

    add interface Flag to PrinterType enum

--- src/main/java/scc/services/printer/PrinterType.java
+++ src/main/java/scc/services/printer/PrinterType.java
@@ -1,9 +1,22 @@
 package scc.services.printer;
 
+import scc.interfaces.Flag;
 import scc.utils.enums.EnumUtils;
 
-public enum PrinterType {
-    PRINT_TO_CONSOLE;
+public enum PrinterType implements Flag {
+    PRINT_TO_CONSOLE(null),
+    PRINT_TO_FILE("flag");
+
+    private final String flag;
+
+    PrinterType(String flag) {
+        this.flag = flag;
+    }
+
+    @Override
+    public String getEnumFlag( ) {
+        return this.flag;
+    }
 
     private static EnumUtils<PrinterType> enumUtils = new EnumUtils<>();
 
