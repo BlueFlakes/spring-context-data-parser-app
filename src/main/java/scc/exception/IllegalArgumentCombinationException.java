95bc0681405d156e6850608d7f39c9e9d9c31905
Kamil <blueflamesxcode@gmail.com>
2018-01-11 12:03:29 +0100

    Add IllegalArgumentCombination exception

---
+++ src/main/java/scc/exception/IllegalArgumentCombinationException.java
@@ -0,0 +1,8 @@
+package scc.exception;
+
+public class IllegalArgumentCombinationException extends Exception {
+
+    public IllegalArgumentCombinationException(String message) {
+        super(message);
+    }
+}
