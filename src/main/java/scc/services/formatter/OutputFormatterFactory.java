a1ead6897fde11144606711c3e9658a810cde45a
Kamil <blueflamesxcode@gmail.com>
2018-01-14 21:27:18 +0100

    change from switch to if's bcs they are null safe instead of switch

--- src/main/java/scc/services/formatter/OutputFormatterFactory.java
+++ src/main/java/scc/services/formatter/OutputFormatterFactory.java
@@ -8,18 +8,19 @@ import scc.models.OrdersInterpreter;
 @Component
 public class OutputFormatterFactory {
     public OutputFormatter createByFormat(OutputFormat outputFormat) throws ImproperArgumentException {
-        switch (outputFormat) {
-            case XML:
-                return new XmlOutputFormatter();
 
-            case JSON:
-                return new JsonOutputFormatter();
-
-            case TABLE:
-                return new TableOutputFormatter();
-
-            default:
-                throw new ImproperArgumentException("Invalid argument delivered");
+        if (outputFormat == OutputFormat.XML) {
+            return new XmlOutputFormatter();
         }
+
+        if (outputFormat == OutputFormat.JSON) {
+            return new JsonOutputFormatter();
+        }
+
+        if (outputFormat == OutputFormat.TABLE) {
+            return new TableOutputFormatter();
+        }
+
+        throw new ImproperArgumentException("Invalid argument delivered: " + outputFormat);
     }
 }
