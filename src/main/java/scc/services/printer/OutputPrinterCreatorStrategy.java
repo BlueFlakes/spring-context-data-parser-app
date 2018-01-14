8251544e3fceca6141eec2a51e2ea9fe5234ef3d
Kamil <blueflamesxcode@gmail.com>
2018-01-14 10:48:49 +0100

    Add Strategy for choosing printer factory

---
+++ src/main/java/scc/services/printer/OutputPrinterCreatorStrategy.java
@@ -0,0 +1,32 @@
+package scc.services.printer;
+
+import org.springframework.stereotype.Component;
+import scc.exception.ImproperArgumentException;
+import scc.exception.ImproperStateException;
+import scc.models.OrdersProvider;
+
+@Component
+public class OutputPrinterCreatorStrategy implements PrinterCreator {
+    private PrinterFactoryProducer factoryProducer;
+
+    public OutputPrinterCreatorStrategy(PrinterFactoryProducer factoryProducer) {
+        this.factoryProducer = factoryProducer;
+    }
+
+    public OutputPrinter createOutputPrinter(OrdersProvider ordersProvider, PrinterType printerType)
+            throws ImproperArgumentException, ImproperStateException {
+
+        PrinterFactories chosenFactory = PrinterFactories.getTypeByOrders(ordersProvider);
+        PrinterAbstractFactory printerAbstractFactory = this.factoryProducer.getPrinterFactory(chosenFactory);
+
+        if (chosenFactory == PrinterFactories.ADVANCED) {
+            return printerAbstractFactory.getOutputPrinterWithUserInputs(printerType, ordersProvider);
+
+        } else if (chosenFactory == PrinterFactories.DEFAULT) {
+            return printerAbstractFactory.getOutputPrinterWithSettledState(printerType);
+
+        } else {
+            throw new ImproperArgumentException("Invalid value delivered");
+        }
+    }
+}
