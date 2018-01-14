06e0d3c673a29be3fb94ac545f48f4fcae01044e
Kamil <blueflamesxcode@gmail.com>
2018-01-14 11:03:15 +0100

    Reduce not used code

--- src/main/java/scc/services/formatter/OutputFormat.java
+++ src/main/java/scc/services/formatter/OutputFormat.java
@@ -3,25 +3,14 @@ package scc.services.formatter;
 import scc.interfaces.Flag;
 import scc.utils.enums.EnumUtils;
 
-public enum OutputFormat implements Flag {
-    JSON(null),
-    XML(null),
-    TABLE(null);
-
-    private final String flag;
-
-    OutputFormat(String flag) {
-        this.flag = flag;
-    }
+public enum OutputFormat {
+    JSON,
+    XML,
+    TABLE;
 
     private static EnumUtils<OutputFormat> enumUtils = new EnumUtils<>();
 
     public static OutputFormat getByName(String name) {
         return enumUtils.getValue(OutputFormat.class, name);
-    }
-
-    @Override
-    public String getEnumFlag( ) {
-        return this.flag;
     }
 }
