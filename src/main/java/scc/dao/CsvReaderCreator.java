06ff3c754ddf2fdd97ff3a3e414e1f91d42fce52
Kamil <blueflamesxcode@gmail.com>
2018-01-14 15:47:16 +0100

    Add option for delivering custom delimiter

--- src/main/java/scc/dao/CsvReaderCreator.java
+++ src/main/java/scc/dao/CsvReaderCreator.java
@@ -1,8 +1,10 @@
 package scc.dao;
 
+import scc.exception.InvalidArgumentCombinationException;
 import scc.models.OrdersInterpreter;
 
 public class CsvReaderCreator implements ReaderSettingsHandler {
+    private static final String defaultDelimiter = ",";
     private OrdersInterpreter ordersInterpreter;
 
     public CsvReaderCreator(OrdersInterpreter ordersInterpreter) {
@@ -11,9 +13,22 @@ public class CsvReaderCreator implements ReaderSettingsHandler {
 
     @Override
     public DataLoader getInstanceWithSettledProperties( ) {
-        String defaultDelimiter = ",";
+        String defaultDelimiter = getDelimiter();
         FileReader fileReader = new FileReader();
         String path = this.ordersInterpreter.getPath();
         return new CsvReader(fileReader, path, defaultDelimiter);
+    }
+
+    private String lookForDelimiterInsideAdditionalSettings() throws InvalidArgumentCombinationException {
+        final String keyPrefix = "d=";
+        return this.ordersInterpreter.getPrefixValueFromAdditionalInputs(keyPrefix);
+    }
+
+    private String getDelimiter() {
+        try {
+            return lookForDelimiterInsideAdditionalSettings();
+        } catch (InvalidArgumentCombinationException e) {
+            return defaultDelimiter;
+        }
     }
 }
