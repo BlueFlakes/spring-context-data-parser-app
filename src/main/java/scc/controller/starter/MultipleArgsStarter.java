28074d3c87fbc782056bb3175071eaaa4348e110
Kamil <blueflamesxcode@gmail.com>
2018-01-14 10:36:53 +0100

    refactor class name and delete unused imports

--- src/main/java/scc/controller/starter/MultipleArgsStarter.java
+++ src/main/java/scc/controller/starter/MultipleArgsStarter.java
@@ -1,12 +1,16 @@
 package scc.controller.starter;
 
 import scc.controller.SimpleConverter;
+import scc.models.InOutInputsAnalyser;
+import scc.models.OrdersInterpreter;
 import scc.models.OrdersProvider;
+import scc.services.formatter.OutputFormat;
+import scc.services.printer.PrinterType;
 
-public class MultipleArgsStarter implements ConverterStarter {
+public class ManyArgsStarter implements ConverterStarter {
     private OrdersProvider ordersProvider;
 
-    public MultipleArgsStarter(OrdersProvider ordersProvider) {
+    public ManyArgsStarter(OrdersProvider ordersProvider) {
         this.ordersProvider = ordersProvider;
     }
 
