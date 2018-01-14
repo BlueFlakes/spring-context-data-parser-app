2c269512a6547a81fce9003f0fd7791ae16e5e5a
Kamil <blueflamesxcode@gmail.com>
2018-01-14 11:03:01 +0100

    Change type of annotation

--- src/main/java/scc/services/daoInputHandlers/DefaultPipelineReader.java
+++ src/main/java/scc/services/daoInputHandlers/DefaultPipelineReader.java
@@ -1,18 +1,18 @@
 package scc.services.daoInputHandlers;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import scc.dao.DataLoader;
-import scc.dao.ReaderCreatorFactory;
 import scc.dao.DataLoaderType;
-import scc.exception.DAOException;
+import scc.dao.ReaderCreatorFactory;
 import scc.dao.ReaderSettingsHandler;
+import scc.exception.DAOException;
 import scc.exception.ImproperArgumentException;
 import scc.models.OrdersProvider;
 import scc.services.document.Document;
 import scc.services.document.DocumentCreator;
 import scc.services.document.DocumentCreatorFactory;
 
-@Component
+@Service
 public class DefaultPipelineReader implements DaoHandler {
     private final DataLoaderType dataLoaderType = DataLoaderType.CSV_READER;
     private ReaderCreatorFactory readerCreatorFactory;
